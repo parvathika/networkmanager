5b63e7b4eda64a1947321ff73c755ac84e40d95c
Parvathi <parvathi.k@tataelxsi.co.in>
2024-10-18 14:50:54 +0530

    latest changes

--- Tests/L1Tests/tests/test_WiFiSignalStrengthMonitor.cpp
+++ Tests/L1Tests/tests/test_WiFiSignalStrengthMonitor.cpp
@@ -1,86 +1,83 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <fstream>
+#include <iostream>
 #include "WiFiSignalStrengthMonitor.h"
+#include "NetworkManagerImplementation.h"
 
-using ::testing::_;
-using ::testing::Invoke;
-using ::testing::Return;
+using namespace std;
+using namespace WPEFramework;
+using namespace WPEFramework::Plugin;
+
 
 class MockNetworkManagerImplementation : public NetworkManagerImplementation {
 public:
-    MOCK_METHOD(void, ReportWiFiSignalStrengthChangedEvent, (const std::string&, const std::string&, Exchange::INetworkManager::WiFiSignalQuality), (override));
+    MOCK_METHOD(void, ReportWiFiSignalStrengthChangedEvent, 
+                (const std::string&, const std::string&, 
+                 INetworkManager::WiFiSignalQuality), 
+                (override));
 };
+
 
 class WiFiSignalStrengthMonitorTest : public ::testing::Test {
 protected:
-    WiFiSignalStrengthMonitor wifiMonitor;
+    WPEFramework::Plugin::WiFiSignalStrengthMonitor monitor;
     MockNetworkManagerImplementation mockManager;
 
     void SetUp() override {
-        _instance = &mockManager;  
+        _instance = &mockManager;
     }
 
     void TearDown() override {
-        _instance = nullptr;  
+     _instance = nullptr;
     }
 };
 
-TEST_F(WiFiSignalStrengthMonitorTest, RetrieveValuesReturnsCorrectValue) {
+#define BUFFER_SIZE 1024
+
+
+TEST_F(WiFiSignalStrengthMonitorTest, RetrieveValues_Success) {
+    char output_buffer[BUFFER_SIZE];
+    std::string result = monitor.retrieveValues("echo ssid=TestSSID", "ssid", output_buffer, sizeof(output_buffer));
+    EXPECT_EQ(result, "TestSSID");
+}
+
+TEST_F(WiFiSignalStrengthMonitorTest, GetSignalData_Connected) {
+    std::string ssid;
+    WiFiSignalQuality quality;
+    std::string strengthOut;
+
+    ssid = "TestSSID";
+    strengthOut = "-55";
+    quality = WiFiSignalQuality::WIFI_SIGNAL_GOOD;
     
-    std::string command = "echo 'ssid=my_wifi'";
-    char outputBuffer[BUFFER_SIZE];
+    monitor.getSignalData(ssid, quality, strengthOut);
 
-    EXPECT_CALL(mockManager, retrieveValues(command.c_str(), "ssid", _, _))
-        .WillOnce(Invoke([&](const char*, const char*, char* buffer, size_t) {
-            strncpy(buffer, "ssid=my_wifi", BUFFER_SIZE);
-            return std::string("my_wifi");
-        }));
-
-    std::string result = wifiMonitor.retrieveValues(command.c_str(), "ssid", outputBuffer, sizeof(outputBuffer));
-    EXPECT_EQ(result, "my_wifi");
+    
+    EXPECT_EQ(ssid, "TestSSID"); 
+    EXPECT_EQ(quality, WiFiSignalQuality::WIFI_SIGNAL_GOOD); 
 }
 
-TEST_F(WiFiSignalStrengthMonitorTest, GetSignalDataHandlesDisconnectedState) {
-    std::string ssid;
-    Exchange::INetworkManager::WiFiSignalQuality quality;
-    std::string strengthOut;
 
-    EXPECT_CALL(mockManager, retrieveValues(rssid_command, "RSSI", _, _))
-        .WillOnce(Return("0")); 
-
-    wifiMonitor.getSignalData(ssid, quality, strengthOut);
-    EXPECT_EQ(quality, Exchange::INetworkManager::WIFI_SIGNAL_DISCONNECTED);
+TEST_F(WiFiSignalStrengthMonitorTest, StartWiFiSignalStrengthMonitor) {
+    monitor.startWiFiSignalStrengthMonitor(1);  
+    EXPECT_TRUE(monitor.isRunning); 
 }
 
-TEST_F(WiFiSignalStrengthMonitorTest, MonitorThreadFunctionSignalsChange) {
-    std::string ssid;
-    std::string strengthOut;
+TEST_F(WiFiSignalStrengthMonitorTest, MonitorThreadFunction) {
+    EXPECT_CALL(mockManager, ReportWiFiSignalStrengthChangedEvent("TestSSID", "-55", WiFiSignalQuality::WIFI_SIGNAL_GOOD)).Times(1);
 
-    EXPECT_CALL(mockManager, retrieveValues(ssid_command, "ssid", _, _))
-        .WillOnce(Return("my_wifi"));
-    EXPECT_CALL(mockManager, retrieveValues(rssid_command, "RSSI", _, _))
-        .WillOnce(Return("-55"));  
-    EXPECT_CALL(mockManager, ReportWiFiSignalStrengthChangedEvent("my_wifi", "-55", Exchange::INetworkManager::WIFI_SIGNAL_EXCELLENT));
-
-    std::thread monitorThread([this]() { wifiMonitor.monitorThreadFunction(1); });
-    monitorThread.detach();  
+    monitor.startWiFiSignalStrengthMonitor(1);
     std::this_thread::sleep_for(std::chrono::seconds(2));
+    monitor.stopThread = true;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    
-    wifiMonitor.stopThread = true;
-    monitorThread.join();
-}
-
-TEST_F(WiFiSignalStrengthMonitorTest, StartWiFiSignalStrengthMonitorAlreadyRunning) {
-    wifiMonitor.startWiFiSignalStrengthMonitor(1); 
-    EXPECT_FALSE(wifiMonitor.stopThread);
-
-    
-    wifiMonitor.startWiFiSignalStrengthMonitor(1);
-    EXPECT_TRUE(wifiMonitor.isRunning); 
-
-    
-    wifiMonitor.stopThread = true;
+    EXPECT_FALSE(monitor.isRunning);
 }
 
 
+int main(int argc, char **argv) {
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
 
